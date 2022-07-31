7357d48c0373c0a3b46413308af91748465ec65e
byht1 <vitalikbyht1@gmail.com>
2022-07-31 17:20:05 +0300

    1

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,1 +1,14 @@
-const axios = require('axios').default;
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://62dd5ef1ccdf9f7ec2c60cc5.mockapi.io';
+
+// export async function methodName(arguments) {
+//   // body
+// }
+
+export const serverData = async () => {
+  const response = await axios.get('/12');
+  const data = await response.data;
+  console.log('🚀 ~ data', data);
+  return data;
+};
