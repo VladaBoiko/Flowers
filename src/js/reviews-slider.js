59658dc001e516e9a48e1b1f137e3e2f25be4045
ViktoriaZinyak <viktoriazinyak@gmail.com>
2022-08-04 18:36:16 +0300

    sliders ok

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,6 +1,30 @@
 import Swiper from 'swiper/swiper-bundle';
 new Swiper('.revies-swiper__container', {
   navigation: {
-    nextEl: '.swiper-btn',
+    nextEl: '.swiper-btn__next',
+    prevEl: '.swiper-btn__prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyOnViewport: true,
+    padeUpDown: true,
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  speed: 800,
+
+  breakpoints: {
+    370: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1300: {
+      slidesPerView: 3,
+    },
   },
 });
