7d8712344d61a414bfb027bee59744f54b53aad1
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-08-07 20:13:28 +0300

    ...

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,19 +1,20 @@
+console.log('api.js');
 const axios = require('axios').default;
 
 // https://parceljs.org/features/development/#hot-reloading
-if (module.hot) {
-  module.hot.accept();
-}
-
-// axios.defaults.baseURL = 'https://62dd5ef1ccdf9f7ec2c60cc5.mockapi.io';
-
-// export async function methodName(arguments) {
-// body
+// if (module.hot) {
+//   module.hot.accept();
 // }
 
-// export const serverData = async () => {
-//   const response = await axios.get('/12');
-//   const data = await response.data;
-//   console.log('🚀 ~ data', data);
-//   return data;
-// };
+axios.defaults.baseURL = 'https://62efe9458d7bc7c2eb8243b2.mockapi.io';
+
+export async function methodName(...arg) {
+  body;
+}
+
+export const serverData = async () => {
+  const response = await axios.get('/12');
+  const data = await response.data;
+  console.log('🚀 ~ data', data);
+  return data;
+};
