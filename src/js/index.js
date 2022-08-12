1c60ae80a9bc9fe5161c61aad04a24b4180bd920
VladaBoiko <bermezha@gmail.com>
2022-08-13 00:31:10 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,27 @@
-import { serverData } from './Api';
+// import { serverData } from './Api';
 
-const refs = {
-  root: document.querySelector('body'),
-};
+// const refs = {
+//   root: document.querySelector('body'),
+// };
 
-console.log('hello');
-const { root } = refs;
+// console.log('hello');
+// const { root } = refs;
 
-async function data() {
-  try {
-    const data = await serverData();
-    const listGoods = await data.map(({ title, photo, price, id }) => {
-      return `<li data-id=${id}>
-                 <img src=${photo} alt=${title} />
-                 <p>${title}</p>
-                 <p>${price} грн.</p>
-                 <button type='button'>Купить</button>
-              </li>`;
-    });
-    root.insertAdjacentHTML('beforeend', listGoods.join(''));
-  } catch (error) {
-    console.log('error');
-  }
-}
+// async function data() {
+//   try {
+//     const data = await serverData();
+//     const listGoods = await data.map(({ title, photo, price, id }) => {
+//       return `<li data-id=${id}>
+//                  <img src=${photo} alt=${title} />
+//                  <p>${title}</p>
+//                  <p>${price} грн.</p>
+//                  <button type='button'>Купить</button>
+//               </li>`;
+//     });
+//     root.insertAdjacentHTML('beforeend', listGoods.join(''));
+//   } catch (error) {
+//     console.log('error');
+//   }
+// }
 
-data();
+// data();
