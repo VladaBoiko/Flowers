ed78f0d31294276a61367f917de5b8e87bba99b7
Veronika Kravchenko <aniregma@gmail.com>
2022-09-01 12:01:37 +0300

    оформлення модалки

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-hero-modal-open]"),
+    closeModalBtn: document.querySelector("[data-hero-modal-close]"),
+    modal: document.querySelector("[data-hero-modal]"),
     body: document.querySelector("body"),
   };
 
