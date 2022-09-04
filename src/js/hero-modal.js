f956792ce363a455b7e8e8ea76f0683ae0919d09
Veronika Kravchenko <aniregma@gmail.com>
2022-09-05 01:04:56 +0300

    change modal js

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,16 +1,35 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-hero-modal-open]"),
-    closeModalBtn: document.querySelector("[data-hero-modal-close]"),
-    modal: document.querySelector("[data-hero-modal]"),
-    body: document.querySelector("body"),
-  };
+const refs = {
+  openModalBtn: document.querySelector("[data-hero-modal-open]"),
+  closeModalBtn: document.querySelector("[data-hero-modal-close]"),
+  modal: document.querySelector('.hero-modal'),
+  backdrop: document.querySelector('.js-hero-backdrop'),
+  body: document.querySelector("body"),
+}
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.body.classList.add('show-modal');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.body.classList.remove('show-modal');
+  refs.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
   }
-})();
+}
+
+function onEscKeyPress(evt) {
+  if (evt.code === 'Escape') {
+    onCloseModal();
+  }
+}
