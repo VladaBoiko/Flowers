17e0fa8c70c5575bbcf43e67bbf0901d89f3d0b7
Veronika Kravchenko <aniregma@gmail.com>
2022-09-06 21:32:36 +0300

    реалізація випадашки
    реалізація появи випадашки при натисканні по фільтру

--- src/js/filter-section.js
+++ src/js/filter-section.js
@@ -1,18 +1,37 @@
 document.body.addEventListener('click', onFilterItemClick);
-
+    
 function onFilterItemClick(evt) {
-    const activeFilterItem = document.querySelector('.filter-item.is-active')
-    if (!evt.target.closest('.filter-item')) {
+    const activeFilterItem = document.querySelector('[data-filter-item].is-active');
+    const activeDropdownList = document.querySelector('[data-filter-item].is-active>ul');
+    
+    
+    if (!evt.target.closest('[data-filter-item]')) {
         if (activeFilterItem) {
-        activeFilterItem.classList.remove('is-active');
+            switchFilterState(activeFilterItem);
+            switchFilterState(activeDropdownList);
         }
         return;
     }
 
-    if (activeFilterItem) {
-        activeFilterItem.classList.remove('is-active');
+    if (evt.target.closest('[data-filter-item].is-active')) {
+        switchFilterState(activeFilterItem);
+        switchFilterState(activeDropdownList);
+        return;
     }
 
-    const nextActiveFilterItem = evt.target.closest('.filter-item');
-    nextActiveFilterItem.classList.add('is-active');
-} 
+    if (activeFilterItem) {
+        switchFilterState(activeFilterItem);
+        switchFilterState(activeDropdownList);
+    }
+
+    const nextActiveFilterItem = evt.target.closest('[data-filter-item]');
+    if (nextActiveFilterItem) {
+        nextActiveFilterItem.classList.add('is-active');
+        const firstActiveDropdownList = document.querySelector('[data-filter-item].is-active>ul');
+        switchFilterState(firstActiveDropdownList)
+    }
+}
+
+function switchFilterState(dropdownList) {
+    dropdownList.classList.toggle('is-active');
+}
