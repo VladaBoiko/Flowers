87339a2cbdd48f4f21ed653401cee99aaa7a58f6
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-09-07 13:35:54 +0000

    Deploying to gh-pages from @ VladaBoiko/Flowers@7c3a92a9e1738ccd849e7067b8b0987d8aec999f 🚀

---
+++ catalog.0ce411a1.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("myBtn").style.display="block":document.getElementById("myBtn").style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=catalog.0ce411a1.js.map
