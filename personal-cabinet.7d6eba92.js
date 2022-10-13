c3001d87116e759b9f8fea2689cbdee9560a42e1
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-10-13 19:48:41 +0000

    Deploying to gh-pages from @ VladaBoiko/Flowers@7a1466d57c39d786ee7cd119eb2e92e65f91ba0b 🚀

---
+++ personal-cabinet.7d6eba92.js
@@ -0,0 +1,2 @@
+!function(){const o={profileForm:document.querySelector("[data-profile-form]"),notifForm:document.querySelector("[data-pers-notif-form]"),profileFormBtn:document.querySelector("[data-profile-form-btn]"),notifFormBtn:document.querySelector("[data-notif-form-btn]")};function t(o){o.preventDefault()}o.profileForm.addEventListener("submit",(function(e){t(e),o.profileFormBtn.blur()})),o.notifForm.addEventListener("submit",(function(e){t(e),o.notifFormBtn.blur()}))}();
+//# sourceMappingURL=personal-cabinet.7d6eba92.js.map
--- personal-cabinet.bcfdd9bd.js
+++
@@ -1,2 +0,0 @@
-!function(){const t={profileForm:document.querySelector("[data-profile]"),notifForm:document.querySelector("[data-pers-notif-form]"),profileFormBtn:document.querySelector("[data-profile-form-btn]"),notifFormBtn:document.querySelector("[data-notif-form-btn]")};function o(t){t.preventDefault()}t.profileForm.addEventListener("submit",(function(e){o(e),t.profileFormBtn.blur()})),t.notifForm.addEventListener("submit",(function(e){o(e),t.notifFormBtn.blur()}))}();
-//# sourceMappingURL=personal-cabinet.bcfdd9bd.js.map
