f8b7d7715ea0331431c78abd9bdd550c5c4cfb3b
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2022-10-14 20:13:27 +0000

    Deploying to gh-pages from @ VladaBoiko/Flowers@c4d60380460845576ce2b15f441a67249dc56bd3 🚀

--- personal-cabinet.e7085cb4.js
+++
@@ -1,2 +0,0 @@
-const o={profileForm:document.querySelector("[data-profile-form]"),notifForm:document.querySelector("[data-pers-notif-form]"),profileFormBtn:document.querySelector("[data-profile-form-btn]"),notifFormBtn:document.querySelector("[data-notif-form-btn]")};function t(o){o.preventDefault()}o.profileForm.addEventListener("submit",(function(e){t(e),o.profileFormBtn.blur()})),o.notifForm.addEventListener("submit",(function(e){t(e),o.notifFormBtn.blur()}));
-//# sourceMappingURL=personal-cabinet.e7085cb4.js.map
