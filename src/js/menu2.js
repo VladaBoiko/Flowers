dd8a35ea5bd425ea142252b871ddb354ac060525
byht1 <vitalikbyht1@gmail.com>
2022-10-16 22:51:35 +0300

    1

---
+++ src/js/menu2.js
@@ -0,0 +1,31 @@
+const navbarMenu = document.querySelector('.menu');
+const burgerMenu = document.querySelector('#burger');
+const headerMenu = document.querySelector('#header');
+
+console.log('🚀 ~ navbarMenu', navbarMenu);
+console.log('🚀 ~ burgerMenu', burgerMenu);
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+  burgerMenu.addEventListener('click', () => {
+    console.log(1);
+    burgerMenu.classList.toggle('is-active');
+    navbarMenu.classList.toggle('is-active');
+  });
+}
+
+// Close Navbar Menu on Click Links
+document.querySelectorAll('.menu-link').forEach(link => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.remove('is-active');
+    navbarMenu.classList.remove('is-active');
+  });
+});
+
+// Fixed Navbar Menu on Window Resize
+// window.addEventListener('resize', () => {
+//   if (window.innerWidth >= 992) {
+//     if (navbarMenu.classList.contains('is-active')) {
+//       navbarMenu.classList.remove('is-active');
+//     }
+//   }
+// });
--- src/js/page/page-reviews.js
+++ src/js/page/page-reviews.js
@@ -1,6 +1,6 @@
 import '../search';
 import '../reviews/star';
-import '../menu';
+import '../menu2';
 import '../reviews/reviews-form';
 import '../reviews/filter';
 import '../reviews/data';
