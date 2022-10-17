1482d147a647a6d7a3f2e9a25ac11e10c95121c7
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-10-17 15:30:32 +0300

    restore for Veronica

---
+++ src/js/auth/login.js
@@ -0,0 +1,59 @@
+import { refs } from './helpers/refs';
+import { logIn } from '../auth';
+import { showAlertText } from './helpers/helpers';
+
+export let TOKEN = 'token';
+export let userName = 'name';
+
+checkActivePage();
+
+export function checkActivePage() {
+  if (refs.loginSection === null) {
+    return;
+  }
+  refs.loginForm.addEventListener('submit', onLoginSubmit);
+}
+
+function onLoginSubmit(evt) {
+  evt.preventDefault();
+
+  refs.loginSubmitBtn.blur();
+
+  const userInfo = { email: null, password: null };
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  userInfo.email = email.value;
+  userInfo.password = password.value;
+
+  if (email.value === '' || password.value === '') {
+    showAlertText(refs.loginFormAlertText, 'Будь ласка, заповніть усі поля');
+    return;
+  }
+  handleLoginRes(userInfo);
+}
+
+export async function handleLoginRes(userData) {
+  const res = await logIn(userData);
+
+  if (res === 401) {
+    showAlertText(refs.loginFormAlertText, 'Електронна пошта або пароль невірні');
+  } else {
+    goToUserCab();
+    localStorage.setItem(TOKEN, res.token);
+    localStorage.setItem(userName, res.name);
+    refs.loginForm.reset();
+  }
+}
+
+function goToUserCab() {
+  refs.loginSection.classList.add('is-hidden');
+  refs.personalCab.classList.remove('is-hidden');
+  refs.headerUserIcon.addEventListener('click', e => e.preventDefault());
+}
+
+// 401 "user does not exist"
+// else if (!res.isActivate) {
+//     console.log("please, check your email for confirm");
+// }
