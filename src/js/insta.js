486016eb548dbd1f793c29f79811098ebca02c32
Veronika Kravchenko <aniregma@gmail.com>
2022-11-21 12:26:26 +0200

    fix

--- src/js/insta.js
+++ src/js/insta.js
@@ -3,13 +3,11 @@ import Swiper from 'swiper/swiper-bundle';
 export const instagramSwiper = new Swiper('.insta-swiper-container', {
     spaceBetween: 20,
     slidesPerView: 'auto',
-    watchOverflow: false,
-})
+    breakpoints: {
+        1340: {
+            slidesPerView: 4,
+        },
+    },
+});
 
-// preventTouchMove();
-// function preventTouchMove() {
-//     if (window.innerWidth >= 1340) {
-//         instagramSwiper.allowTouchMove = false;
-//     }
-// }
 
